b5b7d7e272595d6068907bc7e4966f82cc5573bd
Robin Jamshidi <88508849+chefsApprentice@users.noreply.github.com>
2025-02-09 10:58:45 +0000

    NO KEYS

--- hackaway/src/main/java/uk/hackaway/hackaway/EnvVar.java
+++ hackaway/src/main/java/uk/hackaway/hackaway/EnvVar.java
@@ -1,7 +1,7 @@
 package uk.hackaway.hackaway;
 
 public class EnvVar {
-	public String ANTHROPIC_API_KEY="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";
+	public String ANTHROPIC_API_KEY="YOUR_KEY_HERE";
 	
 	public EnvVar() {}
 	
